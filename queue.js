e012f8d3e7cfa3641e4ba987022052b35fdcd33b
lenuvia <83255484+lenuvia@users.noreply.github.com>
2022-10-09 00:00:41 -0400

    first commit

---
+++ queue.js
@@ -0,0 +1,29 @@
+const LinkedList = require("./linkedList");
+
+class Queue {
+  constructor() {
+    this.list = new LinkedList();
+  }
+
+  enQueue(val) {
+    this.list.push(val);
+  }
+
+  getLength() {
+    return this.list.length;
+  }
+
+  deQueue() {
+    return this.list.shift();
+  }
+
+  peek() {
+    return this.list.head.val;
+  }
+
+  isEmpty() {
+    return this.list.length === 0 ? true : false;
+  }
+}
+
+module.exports = Queue;
